50786f7b56c52f4d39e7685654a59d651be63a2b
Jakub Martin <jakub.wit.martin@gmail.com>
2019-12-30 11:01:55 +0100

    Preparing for release

--- cmd/root.go
+++ cmd/root.go
@@ -1,18 +1,3 @@
-/*
-Copyright © 2019 NAME HERE <EMAIL ADDRESS>
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
 package cmd
 
 import (
@@ -25,6 +10,7 @@ import (
 	homedir "github.com/mitchellh/go-homedir"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
+	"github.com/tj/go-termd"
 
 	"github.com/cube2222/jql/jql/app"
 )
@@ -33,14 +19,10 @@ var cfgFile string
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Use:   "jql",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Use:   "jql <query>",
+	Short: "JSON Query Processor with a Lispy syntax.",
+	Long:  (&termd.Compiler{}).Compile(string(MustAsset("../README.md"))),
+	Args:  cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
 		input := json.NewDecoder(bufio.NewReaderSize(os.Stdin, 4096*16))
 		output := json.NewEncoder(bufio.NewWriterSize(os.Stdout, 4096*16))
@@ -71,10 +53,6 @@ func init() {
 	// will be global for your application.
 
 	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.jql.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
 
 // initConfig reads in config file and ENV variables if set.
--- go.mod
+++ go.mod
@@ -4,8 +4,8 @@ go 1.13
 
 require (
 	github.com/mitchellh/go-homedir v1.1.0
-	github.com/segmentio/encoding v0.1.8
 	github.com/spf13/cobra v0.0.5
 	github.com/spf13/viper v1.6.1
-	github.com/stretchr/testify v1.2.2
+	github.com/stretchr/testify v1.4.0
+	github.com/tj/go-termd v0.0.1
 )
